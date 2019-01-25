a034a693e693d0dca87d76f4b5f35007b93f7fe7
Shaun William Landy <shaunlandy11@gmail.com>
2019-01-25 22:09:51 +0000

    Base scripts

---
+++ Assets/Scripts/Campfire.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Campfire : MonoBehaviour
+{
+    #region Variables
+
+    #endregion
+
+    #region Unity Events
+    private void Awake()
+    {
+
+    }
+    #endregion
+}
