674f5d4c32c5b039bbc3f93f45367210be17d0b0
Shaun William Landy <shaunlandy11@gmail.com>
2019-01-26 13:50:25 +0000

    Home and Outer Boundary
    Home script for players home space, where they place campfire and spawn

--- Assets/Scripts/GridHelper.cs
+++ Assets/Scripts/GridHelper.cs
@@ -6,6 +6,16 @@ using UnityEngine;
 //helper functions that can be used to snap and sort within the grid
 public class GridHelper
 {
+    #region Enums
+    public enum Corner
+    {
+        NORTH_WEST,
+        SOUTH_EAST,
+        NORTH_EAST,
+        SOUTH_WEST
+    }
+    #endregion
+
     private static Vector2 size;
     private static Vector2 extents;
 
@@ -18,6 +28,7 @@ public class GridHelper
 
     public static void Update()
     {
+        return;
         for (float i = -extents.x; i <= extents.x; i += size.x)
         {
             Debug.DrawLine(new Vector3(i, -extents.y), new Vector3(i, extents.y));
@@ -40,5 +51,30 @@ public class GridHelper
         _pos.x = (Mathf.FloorToInt(_pos.x) * size.x) + (size.x * 0.5f);
         _pos.y = (Mathf.FloorToInt(_pos.y) * size.y) + (size.y * 0.5f);
         return _pos;
+    }
+
+    public static void HomeBounds(Corner _corner, int _x, int _y, out Vector2 _centre, out Vector2 _extents)
+    {
+        _centre = Vector2.zero;
+        //Use half extents for ease
+        _extents = new Vector2(_x * size.x * 0.5f, _y * size.y * 0.5f);
+
+        switch (_corner)
+        {
+            case Corner.NORTH_WEST:
+                _centre = new Vector2(-extents.x + _extents.x, extents.y - _extents.y);
+                break;
+            case Corner.SOUTH_EAST:
+                _centre = new Vector2(extents.x - _extents.x, -extents.y + _extents.y);
+                break;
+            case Corner.NORTH_EAST:
+                _centre = new Vector2(extents.x - _extents.x, extents.y - _extents.y);
+                break;
+            case Corner.SOUTH_WEST:
+                _centre = new Vector2(-extents.x + _extents.x, -extents.y + _extents.y);
+                break;
+            default:
+                break;
+        }
     }
 }
