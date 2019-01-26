79ed7fcde2ba818f9232c359bd53f0b33afe6561
Shaun William Landy <shaunlandy11@gmail.com>
2019-01-26 17:38:04 +0000

    Campfire Animator

---
+++ Assets/Scripts/CampfireAnimation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CampfireAnimation : MonoBehaviour
+{
+    #region Variables
+    [SerializeField] private Animator animator = null;
+    [SerializeField] private SpriteRenderer spriteRenderer = null;
+    [SerializeField] private Sprite[] sprites = null;
+    #endregion
+
+    //Called from animator
+    public void SetAnimationIndex(int _index)
+    {
+        spriteRenderer.sprite = sprites[_index];
+    }
+
+    public void Extinguish()
+    {
+        animator.SetTrigger("Extinguish");
+    }
+
+    public void Restart()
+    {
+        animator.SetTrigger("Restart");
+    }
+
+}
