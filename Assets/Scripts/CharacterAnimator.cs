b794f6a38272eb627a247a08f31e47e034a3d7be
Shaun William Landy <shaunlandy11@gmail.com>
2019-01-26 19:20:15 +0000

    Character Animator

---
+++ Assets/Scripts/CharacterAnimator.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimator : MonoBehaviour
+{
+    #region Variables
+    [SerializeField] private Animator animator = null;
+    [SerializeField] private SpriteRenderer spriteRenderer = null;
+    [SerializeField] private Sprite[] sprites = null;
+    [SerializeField] private Texture2D[] normal_textures = null;
+    #endregion
+
+    //Called from animator
+    public void SetAnimationIndex(int _index)
+    {
+        spriteRenderer.sprite = sprites[_index];
+        //spriteRenderer.material.SetTexture("_NORMALMAP", normal_textures[_index]);
+        //Also change material
+    }
+
+    public void Sit()
+    {
+        animator.SetTrigger("Sit");
+    }
+
+    public void Walk()
+    {
+        animator.SetTrigger("WalkAndStop");
+        animator.SetBool("IsMoving", true);
+    }
+
+    public void Stop()
+    {
+        animator.SetTrigger("WalkAndStop");
+        animator.SetBool("IsMoving", false);
+    }
+
+    public void Stab()
+    {
+        animator.SetTrigger("Stab");
+    }
+
+    public void Water()
+    {
+        animator.SetTrigger("Water");
+    }
+}
